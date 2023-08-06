d67299a4f4cd12bfe8165629cc33da3a9c64940c
OGibbard <gibbard.o@oundleschool.org.uk>
2023-08-06 20:28:40 +0100

    Speed up with pickle

--- smaSingleParam.py
+++ smaSingleParam.py
@@ -50,8 +50,11 @@ def singleListTest(paramsList):
     parameters=["200 sma", "100 sma", "50 sma", "20 sma", "10 sma", "current price", "price 1 days after", "price 2 days after", "price 3 days after", "price 4 days after", "price 5 days after", "price 6 days after", "price 7 days after", "price 8 days after", "price 9 days after", "price 10 days after"]
 
     openAverages=[]
+    closeAverages=[]
     for k in range(10,1205,5):
-        openAverages.append(pl.read_csv("../Data/sp500/sma/2017/Close/SimpleMovingAverages"+str(k)+".csv", infer_schema_length=None))
+        openAverages.append(pl.read_csv("../Data/sp500/sma/2017/Open/SimpleMovingAverages"+str(k)+".csv", infer_schema_length=None))
+    for k in range(10,1205,5):
+        closeAverages.append(pl.read_csv("../Data/sp500/sma/2017/Close/SimpleMovingAverages"+str(k)+".csv", infer_schema_length=None))
     stocks=list(openAverages[0].columns)
 
     start=0
@@ -97,8 +100,10 @@ def singlePickleTest(paramsList):
 
     parameters=["200 sma", "100 sma", "50 sma", "20 sma", "10 sma", "current price", "price 1 days after", "price 2 days after", "price 3 days after", "price 4 days after", "price 5 days after", "price 6 days after", "price 7 days after", "price 8 days after", "price 9 days after", "price 10 days after"]
 
-    with open('../Data/sp500/polarsOpenAverages.pkl', 'rb') as file:
+    with open('../Data/sp500/pandasOpenAverages.pkl', 'rb') as file:
         openAverages = pickle.load(file)
+    with open('../Data/sp500/pandasCloseAverages.pkl', 'rb') as file:
+        closeAverages = pickle.load(file)
     with open('../Data/sp500/stockList.pkl', 'rb') as file:
         stocks = pickle.load(file)
 
@@ -110,11 +115,12 @@ def singlePickleTest(paramsList):
     averagePercent=0
 
     for k in range(start, end):
+        openCurrent=closeAverages[k]
         for j in range(len(stocks)):
-            if openAverages[k][paramsList[0],stocks[j]] > openAverages[k][paramsList[1],stocks[j]]:
+            if openCurrent[stocks[j]][paramsList[0]] > openCurrent[stocks[j]][paramsList[1]]:
                 total+=1
-                averagePercent+=((openAverages[k][paramsList[2],stocks[j]])/(openAverages[k][paramsList[3],stocks[j]]))
-                if openAverages[k][paramsList[2],stocks[j]] > openAverages[k][paramsList[3],stocks[j]]:
+                averagePercent+=((openCurrent[stocks[j]][paramsList[2]])/(openCurrent[stocks[j]][paramsList[3]]))
+                if openCurrent[stocks[j]][paramsList[2]] > openCurrent[stocks[j]][paramsList[3]]:
                     momentumWorks+=1
     try:
         averagePercent=averagePercent/total
